a22048693d5f9fb4259de1c68d1a5cf09f8e755a
Vithya Srirajan <vithyasangkavi@gmail.com>
2024-10-05 02:09:04 +0530

    Add new vendors done with passing hard coded token

--- client/src/pages/vendors/add-vendors.jsx
+++ client/src/pages/vendors/add-vendors.jsx
@@ -3,7 +3,7 @@ import AdminHeader from '../../components/admin-header';
 import { Form } from 'react-bootstrap';
 import CoverImage from '../../assets/background-image.jpg';
 import VendorImage from '../../assets/vendors.jpg'; 
-import React from 'react';
+import React, { useState } from 'react'; 
 import {
     MDBContainer,
     MDBRow,
@@ -13,10 +13,45 @@ import {
     MDBCardImage
 } from 'mdb-react-ui-kit';
 import { useNavigate } from 'react-router-dom';
+import axios from 'axios';
 
 function AddVendors() {
-
     const navigate = useNavigate();
+
+    // State to hold form input values
+    const [vendorData, setVendorData] = useState({
+        userName: "",
+        email: "",
+        password: "",
+        address: "",
+        phoneNumber: ""
+    });
+
+    // Update state based on input changes
+    const handleChange = (e) => {
+        const { name, value } = e.target;
+        setVendorData((prevData) => ({
+            ...prevData,
+            [name]: value
+        }));
+    };
+
+    //Add new vendor
+    const addVendor = async (e) => {
+        e.preventDefault(); 
+
+        try {
+            await axios.post('http://localhost:5296/api/admin/vendor/create-vendor', vendorData, {
+                headers: {
+                    'Authorization': `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJuYW1laWQiOiJjOWI1YjUwYi04ODBhLTQ3NjgtYjg1ZC1iZmQ1Njc0ZTAwOTciLCJ1bmlxdWVfbmFtZSI6IkZhaG1pTmV3MSIsImVtYWlsIjoiZmFobWlAdGVzdC5jb20iLCJyb2xlIjpbIlVzZXIiLCJDU1IiLCJBZG1pbiIsIlZlbmRvciJdLCJuYmYiOjE3MjgwNjUxOTYsImV4cCI6MTcyODA2ODc5NiwiaWF0IjoxNzI4MDY1MTk2LCJpc3MiOiJFQUQiLCJhdWQiOiJDdXN0b21lcnMifQ.PEu6Gc6v3rK_TcjASLCo0ek_g_fBoeZjdgbP1wYOezE`
+                }
+            });
+            navigate('/displayVendors');
+
+        } catch (error) {
+            console.error('Error adding vendor:', error);
+        }
+    };
 
     return (
         <div
@@ -44,33 +79,58 @@ function AddVendors() {
                                 {/* Column for Form */}
                                 <MDBCol md='6'>
                                     <h4 className="form-heading">Add New Vendor</h4>
-                                    <Form>
+                                    <Form onSubmit={addVendor}>
                                         <Form.Group className="mb-3" controlId="name">
-                                            <Form.Label>First Name</Form.Label>
-                                            <Form.Control type="text" />
-                                        </Form.Group>
-
-                                        <Form.Group className="mb-3" controlId="lname">
-                                            <Form.Label>Last Name</Form.Label>
-                                            <Form.Control type="text" />
+                                            <Form.Label>User Name</Form.Label>
+                                            <Form.Control 
+                                                type="text" 
+                                                name="userName" 
+                                                value={vendorData.userName} 
+                                                onChange={handleChange}
+                                            />
                                         </Form.Group>
 
                                         <Form.Group className="mb-3" controlId="email">
                                             <Form.Label>Email Address</Form.Label>
-                                            <Form.Control type="email" />
+                                            <Form.Control 
+                                                type="email" 
+                                                name="email"
+                                                value={vendorData.email} 
+                                                onChange={handleChange}
+                                            />
                                         </Form.Group>
 
-                                        <Form.Group className="mb-3" controlId="regno">
-                                            <Form.Label>Registration Number</Form.Label>
-                                            <Form.Control type="text" />
+                                        <Form.Group className="mb-3" controlId="password">
+                                            <Form.Label>Password</Form.Label>
+                                            <Form.Control 
+                                                type="password"
+                                                name="password"
+                                                value={vendorData.password} 
+                                                onChange={handleChange}
+                                            />
                                         </Form.Group>
 
-                                        <Form.Group className="mb-3" controlId="contactno">
+                                        <Form.Group className="mb-3" controlId="address">
+                                            <Form.Label>Address</Form.Label>
+                                            <Form.Control 
+                                                type="text" 
+                                                name="address"
+                                                value={vendorData.address} 
+                                                onChange={handleChange}
+                                            />
+                                        </Form.Group>
+
+                                        <Form.Group className="mb-3" controlId="phoneNumber">
                                             <Form.Label>Contact Number</Form.Label>
-                                            <Form.Control type="text" />
+                                            <Form.Control 
+                                                type="text" 
+                                                name="phoneNumber"
+                                                value={vendorData.phoneNumber} 
+                                                onChange={handleChange}
+                                            />
                                         </Form.Group>
 
-                                        <Button variant="outline-success" className="mt-4 w-100" type="submit" onClick={() => navigate('/displayVendors')}>
+                                        <Button variant="outline-success" className="mt-4 w-100" type="submit">
                                             Add Vendor
                                         </Button>
                                     </Form>
